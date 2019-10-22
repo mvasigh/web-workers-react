bc50c9f688a5bf3810c463fd2fe01e9a13367844
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-10-22 07:58:24 -0500

    wrap worker in promise with asyncFIb

---
+++ vanilla-app/src/asyncFib.js
@@ -0,0 +1,31 @@
+function asyncFib(data) {
+  // Create a Web Worker
+  const fibWorker = new Worker('./fib.worker.js');
+
+  // Return a Promise that resolves to the value returned from Worker
+  return new Promise((resolve, reject) => {
+    // Handle the message event from the worker
+    const handleMessage = event => {
+      const result = event.data; // needs to go back to the consumer
+      resolve(result);
+      fibWorker.removeEventListener('message', handleMessage);
+      fibWorker.terminate();
+    };
+
+    // Handle any errors from the worker
+    const handleError = error => {
+      reject(error);
+      fibWorker.removeEventListener('error', handleError);
+      fibWorker.terminate();
+    };
+
+    // Mount listeners
+    fibWorker.addEventListener('message', handleMessage);
+    fibWorker.addEventListener('error', handleError);
+
+    // Send the data to the worker via postMessage
+    fibWorker.postMessage(data);
+  });
+}
+
+export default asyncFib;
--- vanilla-app/src/index.js
+++ vanilla-app/src/index.js
@@ -1,7 +1,5 @@
 import createBounce from './bounce';
-
-// * This is a Parcel-only way of using Web Workers!
-const fibWorker = new Worker('./fib.worker.js');
+import asyncFib from './asyncFib';
 
 const form = document.getElementById('input-form');
 const input = document.getElementById('input');
@@ -16,20 +14,14 @@ form.addEventListener('submit', e => {
   const value = input.valueAsNumber;
   const startTime = performance.now();
 
-  const handleMessage = event => {
-    // event.data will have our result
-    const result = event.data;
-
-    const endTime = performance.now();
-    const time = (endTime - startTime).toFixed(2);
-    display.textContent = `Done! Result: ${result}, Time: ${time} ms`;
-    input.value = '';
-
-    fibWorker.removeEventListener('message', handleMessage);
-  };
-
-  fibWorker.addEventListener('message', handleMessage);
-  fibWorker.postMessage(value);
+  asyncFib(value)
+    .then(result => {
+      const endTime = performance.now();
+      const time = (endTime - startTime).toFixed(2);
+      display.textContent = `Done! Result: ${result}, Time: ${time} ms`;
+      input.value = '';
+    })
+    .catch(e => console.error(e));
 });
 
 bounce();
