b25cbc7884d23ff91d8e7bc8de40589da5c76558
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-10-24 08:19:24 -0500

    change calculating to loading

--- react-app/src/components/Display.js
+++ react-app/src/components/Display.js
@@ -1,16 +1,11 @@
 import React from 'react';
 
-function Display({
-  result = null,
-  time = null,
-  calculating = false,
-  ...props
-}) {
+function Display({ result = null, time = null, loading = false, ...props }) {
   let displayOutput = `Result: ${result}, time: ${time && time.toFixed(2)} ms`;
 
   return (
-    <p className="Display-result">
-      {calculating
+    <p className="Display-result" {...props}>
+      {loading
         ? 'Calculating...'
         : time && result
         ? `Done! ${displayOutput}`
